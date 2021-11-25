c19fd547e36a0358e9ec1c845f4dea7627e2d462
arkadiusz971 <teh0lzon@gmail.com>
2021-11-25 18:23:43 +0100

    Map centers on user's current location

--- fitness/src/components/leaflet/GetUserPosition.js
+++ fitness/src/components/leaflet/GetUserPosition.js
@@ -1,23 +1,20 @@
-import React, { useState } from 'react'
-import {Marker, Popup, useMapEvents } from 'react-leaflet'
+import React, { useState, useEffect } from 'react'
+import { Marker, useMap } from 'react-leaflet'
 
 const GetUserPosition = () => {
   const [userPosition, setPosition] = useState(null)
-  const map = useMapEvents({
-    click() {
-      map.locate()
-    },
-    locationfound(e) {
-      setPosition(e.latlng)
-      map.flyTo(e.latlng, 15)
-    }
-  })
+  const map = useMap()
 
+  useEffect(() => {
+    map.locate().on('locationfound', e => {
+      setPosition(e.latlng)
+      map.setView(e.latlng, 15)
+      map.stopLocate()
+    })
+  })
+  
   return userPosition === null ? null : (
-    <Marker position={userPosition} >
-      <Popup>
-        You are here
-      </Popup>
+    <Marker position={userPosition}>
     </Marker>
   )
 }
--- fitness/src/components/leaflet/LeafletMap.js
+++ fitness/src/components/leaflet/LeafletMap.js
@@ -1,39 +1,18 @@
 import React from 'react'
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
-import teslaData from '../data/sampleData.json'
 import GetUserPosition from './GetUserPosition'
 
-
 const LeafletMap = () => {
-
-  const filteredStations = teslaData.filter(tesla => tesla.address.country === "France")
-
-
     return (
-        <div id="map">
-            <MapContainer center={[49, 2]} zoom={5} scrollWheelZoom={true}>
-                <TileLayer
-                    attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                />
-                {filteredStations.map(tesla => (
-                    <Marker
-                        key={tesla.id}
-                        position={[tesla.gps.latitude, tesla.gps.longitude]}>
+        <MapContainer center={[49, 2]} zoom={5} scrollWheelZoom={true}>
+            <GetUserPosition />
 
-                        <Popup position={[tesla.gps.latitude, tesla.gps.longitude]}>
-                            <div>
-                                <p>{"Name: " + tesla.name}</p>
-                                <p>{"Status: " + tesla.status}</p>
-                                <p>{"Number of chargers: " + tesla.stallCount}</p>
-                            </div>
-                        </Popup>
 
-                    </Marker>
-                ))}
-                <GetUserPosition />
-            </MapContainer>
-        </div>
+            <TileLayer
+                attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+        </MapContainer>
     )
 }
 export default LeafletMap
